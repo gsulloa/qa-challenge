fcd9cdb499043caf2d72a09c1ef99169c288e070
Gabriel Ulloa <gsulloa@uc.cl>
2018-04-15 05:59:47 -0300

    muchas cosas

--- src/redux/modules/commits.js
+++ src/redux/modules/commits.js
@@ -1,24 +1,28 @@
 import { newError } from "./error"
+import { doFetch } from "./fetching"
 
-const SELECT_COMMIT = 'SELECT_COMMIT'
-const DESELECT_COMMIT = 'DESELECT_COMMIT'
+const SELECT_COMMIT = "SELECT_COMMIT"
+const DESELECT_COMMIT = "DESELECT_COMMIT"
+const SET_COMMITS = "SET_COMMITS"
 
 const type = "GRIDCOMMITS"
 const initialState = {
   fetching: false,
   error: false,
   data: {},
-  selected: [1]
+  selected: [1],
 }
 export default function commits(state = initialState, action) {
   switch (action.type) {
+    case SET_COMMITS:
+      return {
+        ...state,
+        data: action.payload.data,
+      }
     case SELECT_COMMIT:
       return {
         ...state, // state anterior
-        selected: [
-          ...state.selected,
-          action.payload.id
-        ]
+        selected: [...state.selected, action.payload.id],
       }
     case DESELECT_COMMIT: {
       const index = state.selected.findIndex(e => e === action.payload.id)
@@ -26,8 +30,8 @@ export default function commits(state = initialState, action) {
         ...state, // state anterior
         selected: [
           ...state.selected.slice(0, index),
-            ...state.selected.slice(index + 1),
-        ]
+          ...state.selected.slice(index + 1),
+        ],
       }
     }
     case `${type}_FETCH_START`:
@@ -54,12 +58,41 @@ export default function commits(state = initialState, action) {
 /*
   before Actions
 */
+
+function fetchCommits(api) {
+  return api.get("/commits/all")
+}
+
+export function getCommits() {
+  return async (dispatch, getState, api) => {
+    try {
+      const response = await doFetch(
+        dispatch,
+        fetchCommits(api.api.withToken(getState().authentication.data.token)),
+        type
+      )
+      dispatch(dispatch(setCommits({ data: response })))
+    } catch (e) {
+      newError(dispatch, type, { e })
+    }
+  }
+}
+
+function setCommits({ data }) {
+  return {
+    type: SET_COMMITS,
+    payload: {
+      data,
+    },
+  }
+}
+
 export function selectCommit({ commitId }) {
   return {
     type: SELECT_COMMIT,
     payload: {
       id: commitId,
-    }
+    },
   }
 }
 export function deselectCommit({ commitId }) {
@@ -67,6 +100,6 @@ export function deselectCommit({ commitId }) {
     type: DESELECT_COMMIT,
     payload: {
       id: commitId,
-    }
+    },
   }
 }
--- src/redux/modules/repositories.js
+++ src/redux/modules/repositories.js
@@ -1,12 +1,15 @@
 import { newError } from "./error"
-
-const SET_REPOSITORIES = 'SET_REPOSITORIES'
+import { doFetch } from "./fetching"
+const SET_REPOSITORIES = "SET_REPOSITORIES"
+const SELECT_REPO = "SELECT_REPO"
+const ADD_REPOSITORIE = "ADD_REPOSITORIE"
 
 const type = "REPOSITORY"
 const initialState = {
   fetching: false,
   error: false,
   data: [],
+  selected: undefined,
 }
 export default function repositories(state = initialState, action) {
   switch (action.type) {
@@ -14,6 +17,19 @@ export default function repositories(state = initialState, action) {
       return {
         ...state, // state anterior
         data: action.payload.data,
+      }
+    case ADD_REPOSITORIE:
+      return {
+        ...state,
+        data: [
+          ...state.data,
+          action.payload.repo,
+        ]
+      }
+    case SELECT_REPO:
+      return {
+        ...state,
+        selected: action.payload.selected,
       }
     case `${type}_FETCH_START`:
       return {
@@ -38,30 +54,70 @@ export default function repositories(state = initialState, action) {
 /*
    api Fetchs
  */
+function fetchRepos(api) {
+  return api.get("/repositories")
+}
+
+function fetchCreateRepo(api, name) {
+  return api.post("/repositories", { name })
+}
 
 /*
   before Actions
 */
 export function getRepositories() {
   return async (dispatch, getState, api) => {
-    //const response = await doFetch(dispatch, login(api.api, creds), type)
     try {
-      const response = [
-        { name: 'repo'}
-      ]
+      const response = await doFetch(
+        dispatch,
+        fetchRepos(api.api.withToken(getState().authentication.data.token)),
+        type
+      )
       dispatch(setRepositories({ repositories: response }))
     } catch (e) {
-        newError(dispatch, type, { e })
+      newError(dispatch, type, { e })
     }
   }
 }
 
+export function createRepo(name) {
+  return async (dispatch, getState, api) => {
+    debugger
+    try {
+      const response = await doFetch(
+        dispatch,
+        fetchCreateRepo(api.api.withToken(getState().authentication.data.token), name),
+        type
+      )
+      dispatch(getRepositories())
+    } catch (e) {
+      newError(dispatch, type, { e })
+    }
+  }
+}
 
 function setRepositories({ repositories }) {
   return {
     type: SET_REPOSITORIES,
     payload: {
       data: repositories,
+    },
+  }
+}
+function addRepository(response) {
+  return {
+    type: ADD_REPOSITORIE,
+    payload: {
+      repo: response,
     }
+  }
+}
+
+export function selectRepository({ repoId }) {
+  return {
+    type: SELECT_REPO,
+    payload: {
+      selected: repoId,
+    },
   }
 }
